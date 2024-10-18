83543c29ccbdeccbd0371218df0468c740d6f9e7
¨antonio¨ <aramireza15@miumg.edu.gt>
2024-10-18 02:42:55 -0600

    creacion de entorno-base de datos-login-roles

---
+++ src/app/dashboard/admin/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function AdminDashboard() {
+  return (
+    <div className="h-[calc(100vh-7rem)] flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-white">Admin Dashboard</h1>
+    </div>
+  );
+}
